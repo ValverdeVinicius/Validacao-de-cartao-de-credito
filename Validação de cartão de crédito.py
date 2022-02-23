bd30ae0844fe91f72ca6f2351873c4b7ff069f4f
Vinicius Valverde Cardoso <97487221+ValverdeVinicius@users.noreply.github.com>
2022-02-22 22:49:45 -0300

    Update Validação de cartão de crédito.py

--- Validação de cartão de crédito.py
+++ Validação de cartão de crédito.py
@@ -1,15 +1,29 @@
-def validate(card_number):
-    def digits_of(n):
-        return [int(d) for d in str(n)]
-    digits = digits_of(card_number)
-    odd_digits = digits[-1::-2] #ímpar
-    even_digits = digits[-2::-2] #par
-    checksum = 0
-    checksum += sum(odd_digits)
-    for d in even_digits:
-        checksum += sum(digits_of(d*2))
-    return checksum % 10
-if validate(input('Insira o numero: \n')) == 0:
-    print('Valido')
+card_number = list(input("Digite o número do cartão: ").strip())
+
+# Remove o último dígito do cartão
+check_digit = card_number.pop()
+
+# Inverter a ordem dos números restantes
+card_number.reverse()
+
+processed_digits = []
+
+for index, digit in enumerate(card_number):
+	if index % 2 == 0:
+		doubled_digit = int(digit) * 2
+
+		# Subtrair 9 de qualquer resultados que sejam superiores a 9		
+		if doubled_digit > 9:
+			doubled_digit = doubled_digit - 9
+
+		processed_digits.append(doubled_digit)
+	else:
+		processed_digits.append(int(digit))
+
+total = int(check_digit) + sum(processed_digits)
+
+# Verificar se a soma dos dígitos é divisível por 10
+if total % 10 == 0:
+	print("Valido!")
 else:
-    print('Invalido')
+	print("Invalido!")
